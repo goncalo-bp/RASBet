d8bbf86c77c1c46cb9b525b77eb74a888e1ed7af
Private-053 <hugojbrandao@gmail.com>
2022-11-28 13:48:19 +0000

    cenas do boletim

--- react-website-rasbet/src/components/Boletim.js
+++ react-website-rasbet/src/components/Boletim.js
@@ -5,84 +5,11 @@ import { Button } from './Button';
 
 export default function Form_L() {
 
-// States for registration
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [newPassword, setNewPassword] = useState('');
 
-// States for checking the errors
-const [error, setError] = useState(0); // 0 - incompleto | 1 - mail/pass incorretos
-const [btnPopup, setBtnPopup] = useState(false);
-
-// Handling the email change
-const handleEmail = (e) => {
-	setEmail(e.target.value);
-};
-
-// Handling the password change
-const handlePassword = (e) => {
-	setPassword(e.target.value);
-};
-
-// Handling the password change
-const handleNewPassword = (e) => {
-	setNewPassword(e.target.value);
-};
-
-function toJson(email,password,newPassword) {
-	return {
-		"email": email,
-		"password": password,
-		"newPassword": newPassword
-	}
-}
-
-const handleSubmit = (e) => {
-	e.preventDefault();
-	if (newPassword === '' || email === '' || password === '') {
-	setError(0);
-	setBtnPopup(true);
-	}
-	else {
-
-	setError(false);
-	fetch('http://localhost:5002/login', {  // Enter your IP address here
-
-	method: 'POST', 
-	mode: 'cors', 
-	body: JSON.stringify(toJson(email,password, newPassword)), // body data type must match "Content-Type" header
-	headers: {"Content-Type": "application/json"}
-	})
-	.then( (response) => {
-		if(!response.ok) {
-			throw Error(response.status);
-		}
-		else return response.json();
-	}).then( (data) => {
-		window.location.replace('http://localhost:3000/home');
-	})
-	.catch( (error,status) => {
-		console.log("error: ",status);
-		setError(1);
-		setBtnPopup(true);
-	});
-	}
-};
-const errorMessage = () => {
-	return (
-	<div>
-		{error === 0 ? <h1>Complete todos os campos</h1> :
-		<h1>Email / Password incorretos</h1>}
-	</div>
-	);
-};
 
 return (
 <div className="edit-fundo">
-	<form className='edit-content'>
-		<Popup trigger={btnPopup} setTrigger={setBtnPopup}>
-			{errorMessage()}
-		</Popup>
+	<form className='edit-content-boletim'>
 		<div className='edit-header'>
 			<h1 style = {{fontSize:'70px'}}> Boletim</h1>
 			<br/>
@@ -92,8 +19,13 @@ return (
 			<Button className="btn--primary--gray--medium">Depositar</Button>
 		</div>
 		<br/>
-        <div className='edit-tipo-aposta'>
+        <div className='edit-tipo-jogo'>
+			<span>Sporting - Varzim</span>
+			<span><Button className='btn--x--gray--medium'>X</Button></span>
         </div>
+		<div className='edit-tipo-odds'>
+			<span>Resultado: Empate</span>
+		</div>
 	</form>
 </div>
 );
