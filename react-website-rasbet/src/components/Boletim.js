f8e4d6f5fc99929a8cad32fe42ca6884b3f48899
Private-053 <hugojbrandao@gmail.com>
2022-11-29 15:29:01 +0000

    boletim

--- react-website-rasbet/src/components/Boletim.js
+++ react-website-rasbet/src/components/Boletim.js
@@ -5,7 +5,17 @@ import { Button } from './Button';
 
 export default function Form_L() {
 
+const [value, setValue] = useState('');
 
+
+const [error, setError] = useState(0); // 0 - incompleto | 1 - mail/pass incorretos
+const [btnPopup, setBtnPopup] = useState(false);
+
+
+// Handling the password change
+const handleValue = (e) => {
+	setValue(e.target.value);
+};
 
 return (
 <div className="edit-fundo">
@@ -25,6 +35,16 @@ return (
         </div>
 		<div className='edit-tipo-odds'>
 			<span>Resultado: Empate</span>
+			<span className='edit-odd'>3.5</span>
+		</div>
+		<br/>
+        <div className='edit-tipo-jogo'>
+			<span>Sporting - Varzim</span>
+			<span><Button className='btn--x--gray--medium'>X</Button></span>
+        </div>
+		<div className='edit-tipo-odds'>
+			<span>Resultado: Empate</span>
+			<span className='edit-odd'>3.5</span>
 		</div>
 	</form>
 </div>
