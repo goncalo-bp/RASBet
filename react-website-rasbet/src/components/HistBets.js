a6429aa5fb1c3b94606780099d24d3fa3bc2a052
TBlanc157 <gmpinto11@gmail.com>
2022-12-01 04:42:07 +0000

    historico apostas non responsive TODO mudar botão quando ativo

--- react-website-rasbet/src/components/HistBets.js
+++ react-website-rasbet/src/components/HistBets.js
@@ -6,7 +6,15 @@ import Table from "./Table";
 
 function HistoricoApostas() {
     const [id, setId] = useState(1); //TODO - ir buscar o id do utilizador
-    const [tableData, setTableData] = useState([]);
+
+    const [simples, setSimples] = useState([]);
+    const [multipla, setMultipla] = useState([]);
+
+    const [btnSimples, setBtnSimples] = useState(false);
+    const [btnMultipla, setBtnMultipla] = useState(false);
+
+    const [empty, setEmpty] = useState({control : false});
+
     const [nome, setNome] = useState("Carlos Pereira");
 
     const [button,setButton] = useState(true);
@@ -44,7 +52,9 @@ function HistoricoApostas() {
             else return response.json();
         }).then((data) => {
             console.log(data);
-            //setTableData(data);
+            setSimples(data);
+            {data.length === 0 ? setEmpty({control : true, msg : 'Ainda não fez apostas simples.'}) : setEmpty({control : false})}
+            {data.length > 0 ? setBtnSimples(true) : setBtnSimples(false)}
         })
         .catch(error => {
             console.log("error: ", error);
@@ -66,17 +76,24 @@ function HistoricoApostas() {
             else return response.json();
         }).then((data) => {
             console.log(data);
-            //setTableData(data);
+            setMultipla(data);
+            {data.length === 0 ? setEmpty({control : true, msg : 'Ainda não fez apostas múltiplas.'}) : setEmpty({control : false})}
+            {data.length ? setBtnMultipla(true) : setBtnMultipla(false)}
         })
         .catch(error => {
             console.log("error: ", error);
         });
     };
 
-    useEffect(() => {
+    const handleSimples = (e) => {
         getSimples();
+        setBtnMultipla(false);
+    };
+
+    const handleMultipla = (e) => {
         getMultiplas();
-    }, []);
+        setBtnSimples(false);
+    };
 
     return (
         <div className='container'>
@@ -93,16 +110,81 @@ function HistoricoApostas() {
                 <br/>
                 <span>
                     <div className='bet-type'>
-                        <Button className='btn--outline--full--orange--large'>
+                        <Button className='btn--outline--full--orange--large' onClick={handleSimples}>
                             Simples
                         </Button>
-                        <Button className='btn--outline--full--orange--large'>
+                        <Button className='btn--outline--full--orange--large' onClick={handleMultipla}>
                             Múltiplas
                         </Button>
                     </div>
                 </span>
-                <div className='table-container'>
-                    
+                <br/><br/>
+                <div className='bets-container'>
+                    {btnSimples && 
+                        <div className='entry'>
+                            <div>
+                                {simples.jogo.map((value, index) => {
+                                    return(
+                                        <div className='jogo'>
+                                            <a className='nome-jogo'>
+                                                <h3>{value[0]}</h3>
+                                            </a>
+                                            <a className='resultado'>
+                                                {value[1] == 'Draw' ? `Resultado: Empate` : `Vencedor: ${value[1]}`}
+                                            </a>
+                                        </div>
+                                    )})
+                                }
+                            </div>
+                            <div className='valores'>
+                                <div className='valores-text'>
+                                    Montante Apostado: <a className='montante'>{simples.montante}€</a>
+                                </div>
+                                <br/>
+                                <div className='valores-text'>
+                                    Total de banhos: <a className='ganho'>{simples.ganho}€</a>
+                                </div>
+                            </div>
+                        </div>
+                    }
+                    {btnMultipla && 
+                        <div>
+                            {multipla.map((entry, index) => {
+                                return(
+                                    <div className='entry'>
+                                        <div>
+                                            {entry.jogo.map((value, index) => {
+                                                return(
+                                                    <div className='jogo'>
+                                                        <a className='nome-jogo'>
+                                                            <h3>{value[0]}</h3>
+                                                        </a>
+                                                        <a className='resultado'>
+                                                            {value[1] == 'Draw' ? `Resultado: Empate` : `Vencedor: ${value[1]}`}
+                                                        </a>
+                                                    </div>
+                                                )})
+                                            }
+                                        </div>
+                                        <div className='valores'>
+                                            <div className='valores-text'>
+                                                Montante apostado: <a className='montante'>{entry.montante}€</a>
+                                            </div>
+                                            <br/>
+                                            <div className='valores-text'>
+                                                Total de ganhos: <a className='ganho'>{entry.ganho}€</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )})
+                            }
+                        </div>
+                    }
+                    {empty.control && 
+                        <div className='empty-message'>
+                            <h3>{empty.msg}</h3>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
